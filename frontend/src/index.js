a7772eddfe7ba2a5a6619876267307f9f83d193e
Bisma Khomeini <bismakhomeini@Bismas-MacBook-Pro.local>
2024-04-20 00:21:29 +0700

    push

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './hooks/useAuth';
-import './styleguide.css';
-import './style.css';
-import AppRouter from './route/Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AuthProvider } from "./hooks/useAuth";
+import "./styleguide.css";
+import "./style.css";
+import AppRouter from "./route/Router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<AuthProvider>
-			<AppRouter />
-		</AuthProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  </React.StrictMode>
 );
